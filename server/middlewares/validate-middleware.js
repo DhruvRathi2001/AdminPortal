37c42482cf56c0897084b31e121e433e332acf38
DHRUV RATHI <rathidhruv9285@gmail.com>
2023-11-26 02:10:13 +0530

    fourth commit

---
+++ server/middlewares/validate-middleware.js
@@ -0,0 +1,25 @@
+//ParsAsync ek inbuilt fubction hota hai jo compare krke mai help keta hai...
+//we will passed "schema" as a parameter here, which is compared with the client data..
+
+// In the context of Express.js or similar web frameworks, "next"() is a function that is passed to middleware functions and is used to pass control to the next middleware or route handler in the chain.
+
+// Here's how it typically works:
+
+// When a request is made to a server, it goes through a series of middleware functions and route handlers.
+// Each middleware or route handler has access to the next function.
+// When a middleware or route handler completes its work, it can call next() to pass control to the next middleware or route handler in the sequence.
+// If next() is not called, the request may get stuck, and subsequent middleware or route handlers won't be executed.
+// It's a way of saying, "I'm done with my part; let the next middleware or route handler take over." It's crucial for the flow of control in an Express application.
+
+const validate = (schema) => async (req, res, next) => {
+  try {
+    const parseBody = await schema.parseAsync(req.body);
+    req.body = parseBody;
+    next();
+  } catch (error) {
+    const message = error.errors[0].message
+    res.status(400).json({ msg: message });
+  }
+};
+
+module.exports = validate;
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const authController = require("../controllers/auth-controller")
+const authController = require("../controllers/auth-controller");
 
-router.route("/").get(authController.home)
-router.route("/register").post(authController.register)
-router.route("/login").post(authController.login)
+const signupSchema = require("../validators/auth-validator");
+const validate = require("../middlewares/validate-middleware");
 
-module.exports = router
+router.route("/").get(authController.home);
+//middleware
+router.route("/register").post(validate(signupSchema), authController.register);
+router.route("/login").post(authController.login);
+
+module.exports = router;
